f2ae88543bd4f195cbaa0e50f24141eb5bb5aeee
tcareer34 <tcareer@hotmail.com>
2025-03-10 20:57:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Tim Nguyen")
+    print("Server Automator v0.1 by Tim Nguyen")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
-    server = Server('ec2-35-85-53-16.us-west-2.compute.amazonaws.com', 'D:\Downloads\Tim_key.pem')
+    server = Server('ubuntu@35.85.53.16', 'C:\Tim_key.pem')
 
     # TODO - Call Ping method and print the results
     command = 'sudo apt update && sudo apt upgrade -y'
