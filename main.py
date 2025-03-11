6a16931c801b9e2a655e3855def9e35f04b82bee
tcareer34 <tcareer@hotmail.com>
2025-03-10 23:42:38 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -11,13 +11,24 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    server = Server('ubuntu@172.31.31.193', 'C:\Tim_key.pem')
 
-    # TODO - Call Ping method and print the results
-    command = 'sudo apt update && sudo apt upgrade -y'
-    # Call Ping and print results
-    if server.ping():
-        print("Updating server...")
-        # server.run_command(command)
-        ssh_result = server.run_command(command)
+    # Server details
+    server_ip = "34.219.149.76"
+    # Key path location
+    rsa_key = r"C:\tnguyen_335_ppk.ppk"
+    username = "ubuntu"
+
+# run application update and upgrade commands
+    upgrade_command = "sudo apt update && sudo apt upgrade -y"
+    my_server = Server(server_ip, rsa_key, username)
+
+# ping and print results
+if my_server.ping():
+    print(f"Server {server_ip} is reachable.")
+    print('Updating...')
+    ssh_result = my_server.run_command(upgrade_command)
+#   print("Upgrade output: \n", ssh_result)
+else:
+    print(f"Server {server_ip} is unreachable.")
+
+
